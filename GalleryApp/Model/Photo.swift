45347a218d35948f9dbf3ececf5a56eb1f46faec
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-08-16 18:03:59 +0200

    added api keys into info.plist added photos repository added photo entity

---
+++ GalleryApp/Model/Photo.swift
@@ -0,0 +1,33 @@
+//
+//  Photo.swift
+//  GalleryApp
+//
+//  Created by Dmitrii Grigorev on 16.08.25.
+//
+
+import Foundation
+
+struct Photo: Identifiable, Hashable {
+    let id: String
+    let slug: String
+    let createdAt: Date
+    let updatedAt: Date
+    let promotedAt: Date?
+    let width: Int
+    let height: Int
+    let color: String
+    let description: String?
+    let altDescription: String?
+    let urls: Self.PhotoURLs
+    let isFavourite: Bool = false
+
+    
+    struct PhotoURLs: Hashable {
+        let raw: String
+        let full: String
+        let regular: String
+        let small: String
+        let thumb: String
+        let smallS3: String
+    }
+}
---
+++ GalleryApp/Repo/PhotosRepository.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosRepository.swift
+//  GalleryApp
+//
+//  Created by Dmitrii Grigorev on 15.08.25.
+//
+
+import Foundation
+
+
+protocol PhotosRepository {
+    func listPhotos(page: Int, perPage: Int) async throws -> [Photo]
+}
